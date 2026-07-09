8a24fa0dfc27d9b770ebe69a96e27d51778a700b
HieuNguyene <nguyenminhhieu1252005@gmail.com>
2026-07-09 23:38:02 +0700

    Delete exp5_7.js

--- exp5_7.js
+++
@@ -1,5 +0,0 @@
-function renderStudentCard({name, score, major}){
-    return `<div>Sinh vien: ${name} - Nganh: ${major} - Diem:${score} <div>`;
-}
-
-console.log(renderStudentCard({name:'Nguyen Minh Hieu',major:'Cong nghe thong tin',score:10}));
